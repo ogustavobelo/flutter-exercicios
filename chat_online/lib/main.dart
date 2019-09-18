c8c745ce8bba0bb0cb607f34efa8eccb462b805b
Gustavo Belo <gustavobelo123@gmail.com>
2019-09-18 12:57:52 -0300

    create chat firebase project

---
+++ chat_online/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
